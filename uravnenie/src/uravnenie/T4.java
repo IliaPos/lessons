0e5f52317b8b34f9e7de07dc5b4722f07e8e3287
Ilya Pospelov <ilya.pospelov2000@gmail.com>
2016-11-14 18:00:09 +0300

    new

--- uravnenie/src/uravnenie/T4.java
+++ uravnenie/src/uravnenie/T4.java
@@ -1,5 +1,36 @@
 package uravnenie;
 
-public class T4 {
+import java.util.Scanner;
 
+public class T4 {
+	public static void main(String[] args) {
+		System.out.println("¬ведите число:");
+		double a = getNumber();
+		if  (a<0){
+			System.out.println(-1);
+			
+		}  else {
+			if(a==0){
+				System.out.println(0);
+				
+			} else{
+				System.out.println(1);
+			}
+		}
+
+	}
+		
+		private static double getNumber() {
+
+			Scanner s = new Scanner(System.in);
+			String str1;
+			while(true) {
+				str1 = s.nextLine();
+				if(str1 != null && !"".equals(str1) ) {
+					return Double.parseDouble(str1);
+				} else{
+					System.err.println("ERROR!!!");
+				}
+			}
+	}
 }
