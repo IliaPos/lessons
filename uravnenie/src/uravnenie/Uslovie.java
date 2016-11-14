e125287ee48abe43e56c096eb47ae708404036c2
Ilya Pospelov <ilya.pospelov2000@gmail.com>
2016-11-14 18:39:31 +0300

    new

--- uravnenie/src/uravnenie/Uslovie.java
+++ uravnenie/src/uravnenie/Uslovie.java
@@ -20,7 +20,7 @@ public class Uslovie {
 		String str1;
 		while(true) {
 			str1 = s.nextLine();
-			if(str1 != null && !"".equals(str1) && str1.matches("^\\d+\\.?\\d*$")) {
+			if(str1 != null && !"".equals(str1) && str1.matches("^-?\\d+\\.?\\d*$")) {
 				return Double.parseDouble(str1);
 			} else{
 				System.err.println("ERROR!!!");
